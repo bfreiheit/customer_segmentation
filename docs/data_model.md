bda95be33c629dad364075f781da11406aaa9851
bfreiheit <belinda.freiheit@gmail.com>
2025-06-24 18:52:46 +0200

    improved data model

---
+++ docs/data_model.md
@@ -0,0 +1,50 @@
+## Data model for 
+
+
+**cohort definition**
+
+| criterion | logic | intention |  
+| --- | --- | --- |  
+| registration time | sign_up_date <= max_session_date - INTERVAL '6 months' | only user with at least 6 months history |  
+| activity duration | max_session_date - min_session_date >= 30 | only user with min. one active month |  
+| recency | max_session_date - last_trip_date >= 180 | only user without new bookings |  
+| time frame restriction | sign_up_date >= max_session_date - INTERVAL '12 months' | only user who registered 12 months ago |
+
+
+*users with at least one booking:*  
+- users are included who registered 6 months ago
+- users are excluded who booked within last 6 months 
+- users are included who are active for at least 1 month  
+
+*users with no bookings:*
+- users are included who have at least ten page clicks  
+- users are included who are active for at least 1 month  
+- users are included who registered 6 months ago
+
+---
+**target groups**
+
+>**1. users are prone to churn -> there last booking is more than six months ago**  
+
+>**2. user who have been active since six months, but did not booked yet**
+
+Further investigation:  
+
+- Check metrics and user ages over time  
+- group user by first session month and track activity duration
+
+**revenue-based KPIs**
+
+| metric | description | formula | date type |
+| --- | --- | --- |  --- |  
+| total_sales | total value of user: distinguish high and low-value user | SUM(hotel + flight cost) | float |  
+
+
+
+| metric | description | formula | date type | 
+| --- | --- | --- |  --- |  
+| min_user_date | first session date per user: serves as user cohort | MIN(s.session_start::date) | date |  
+| max_user_date | last session date per user | MAX(s.session_start::date) | date |  
+| last_trip_date | last booking date per user |  MAX(CASE WHEN s.trip_id IS NOT NULL THEN s.session_start::date END) | date  |  
+| last_trip_user_age | days between last session and last booking per user | MAX(s.session_start::date) - last_trip_date | int |   
+| days_active_age | how long was the user active | MAX(s.session_start::date) - MIN(s.session_start::date) | int |  
--- src/customer_segmentation/utils/queries.py
+++ src/customer_segmentation/utils/queries.py
@@ -1,98 +1,143 @@
 
 
-query_user = '''WITH max_date AS (
+query_user = '''
+WITH max_date AS (
     SELECT MAX(session_start::date) AS max_session_date FROM sessions
 ),
 
 hotels AS (
     SELECT 
-        trip_id,
-        check_out_time::date - check_in_time::date AS nights,
-        rooms,         
+        trip_id,         
+        CASE WHEN check_out_time::date - check_in_time::date <= 0 
+        THEN 1 ELSE check_out_time::date - check_in_time::date 
+        END as nights, 
+
+        rooms,      
         hotel_per_room_usd
-    FROM hotels
-    WHERE 
-        check_out_time::date - check_in_time::date BETWEEN 1 AND 30             
+    FROM hotels            
 ),
 
 flights AS (
     SELECT 
         trip_id,
-        return_time::date - departure_time::date as travel_days,       
-        checked_bags,
-        seats,
-        base_fare_usd
-    FROM flights        
-)
+        CASE WHEN return_time::date - departure_time::date <= 0 
+        THEN 1 
+        ELSE return_time::date - departure_time::date 
+        END as travel_days,         
 
+        CASE WHEN return_flight_booked 
+        THEN ROUND(base_fare_usd / 2, 2)
+        ELSE ROUND(base_fare_usd, 2)
+        END AS flight_price,
+       
+        CASE WHEN return_flight_booked 
+        THEN ROUND(base_fare_usd / seats / 2, 2) 
+        ELSE ROUND(base_fare_usd / seats, 2)
+        END AS flight_price_per_person,
+
+        seats,
+        base_fare_usd,
+        destination_airport_lat,
+        destination_airport_lon
+    FROM flights        
+),
+
+trip_agg AS (
+  SELECT 
+    trip_id,
+    user_id,
+    MAX(CASE WHEN cancellation THEN 1 ELSE 0 END) AS is_cancelled,
+    MIN(session_start::date) AS trip_date
+  FROM sessions
+  WHERE trip_id IS NOT NULL
+  GROUP BY trip_id, user_id
+),
+
+trip_agg_with_lag AS (
+  SELECT
+    trip_id,
+    user_id,
+    is_cancelled,
+    trip_date,
+    LAG(trip_date) OVER (PARTITION BY user_id ORDER BY trip_date) AS prev_trip_date
+  FROM trip_agg
+),
+
+trip_level AS (
+    SELECT t.trip_id,
+    t.user_id,
+    t.is_cancelled,
+    t.trip_date,
+    t.prev_trip_date,
+    f.seats,
+    f.travel_days,
+    f.base_fare_usd,
+    f.flight_price,
+    f.flight_price_per_person,
+    f.destination_airport_lat,
+    f.destination_airport_lon,
+    h.rooms,
+    h.nights,
+    h.hotel_per_room_usd
+    FROM trip_agg_with_lag t
+    LEFT JOIN flights f ON t.trip_id = f.trip_id
+    LEFT JOIN hotels h ON t.trip_id = h.trip_id
+)
 SELECT 
     s.user_id,
+    -- user info
     (md.max_session_date - u.birthdate::date) / 365 AS age,    
-    CASE WHEN u.gender = 'F' THEN 1 ELSE 0 END AS is_female,    
-    CASE WHEN u.married THEN 1 ELSE 0 END AS is_married,
     CASE WHEN u.has_children THEN 1 ELSE 0 END AS has_children,
-
-    SUM(CASE WHEN s.trip_id IS NULL THEN 0 ELSE 1 END) AS cnt_trips,
-    COUNT(s.session_id) AS cnt_sessions,
+    -- trip info
+    ROUND(COALESCE(SUM(CASE WHEN t.trip_id IS NOT NULL THEN 1 ELSE 0 END), 0)) AS cnt_trips, 
+    ROUND(COALESCE(SUM(CASE WHEN t.trip_id IS NOT NULL THEN t.is_cancelled ELSE 0 END), 0)) AS cnt_cancellations,  
+    ROUND(COALESCE(AVG(t.trip_date - t.prev_trip_date), 0)) as avg_diff_trip_days,
+    COALESCE(MAX(s.session_start::date) - MAX(CASE WHEN t.trip_date IS NOT NULL THEN t.trip_date END), 0) AS last_trip_age,
+    MAX(CASE WHEN t.trip_date IS NOT NULL THEN t.trip_date END) AS last_trip_date,
+    -- flight info  
+    ROUND(COALESCE(SUM(t.seats), 0)) AS sum_seats,
+    ROUND(COALESCE(SUM(t.travel_days), 0)) AS sum_travel_days,   
+    ROUND(COALESCE(SUM(t.flight_price), 0), 2) AS sum_flight_price, 
+    ROUND(COALESCE(AVG(6371 * acos(
+        cos(radians(u.home_airport_lat)) * cos(radians(t.destination_airport_lat)) * 
+        cos(radians(u.home_airport_lon) - radians(t.destination_airport_lon)) +
+        sin(radians(u.home_airport_lat)) * sin(radians(t.destination_airport_lat))
+    )), 0)) AS avg_distance_km,
+    -- hotel info
+    ROUND(COALESCE(SUM(t.rooms), 0)) AS sum_rooms,    
+    ROUND(COALESCE(SUM(t.nights), 0)) AS sum_nights,     
+    ROUND(COALESCE(SUM(t.hotel_per_room_usd * t.rooms * t.nights), 0), 2) AS sum_hotel_price,    
+   -- session info
+    COUNT(DISTINCT s.session_id) AS cnt_sessions,
     SUM(s.page_clicks) AS page_clicks,
-
-    SUM(CASE WHEN s.cancellation = TRUE THEN 1 ELSE 0 END) AS cnt_cancellations,
-    SUM(CASE WHEN s.flight_booked = TRUE THEN 1 ELSE 0 END) AS cnt_booked_flights,
-    SUM(CASE WHEN s.hotel_booked = TRUE THEN 1 ELSE 0 END) AS cnt_booked_hotels,
-    
-    SUM(CASE WHEN s.flight_discount = TRUE THEN 1 ELSE 0 END) AS cnt_flight_discount,
-    SUM(CASE WHEN s.hotel_discount = TRUE THEN 1 ELSE 0 END) AS cnt_hotel_discount,
-  
-    MIN(s.session_start::date) AS min_user_date,
     MAX(s.session_start::date) AS max_user_date,
-    MAX(s.session_start::date) - MIN(s.session_start::date) AS days_active_age,
-    MAX(CASE WHEN s.trip_id IS NOT NULL THEN s.session_start::date END) AS Last_trip_date,
-    MAX(CASE WHEN s.trip_id IS NOT NULL THEN s.session_start::date END) -
-    MIN(CASE WHEN s.trip_id IS NOT NULL THEN s.session_start::date END) AS avg_diff_trip_days,
-
-    COALESCE(MAX(s.session_start::date) - 
-             MAX(CASE WHEN s.trip_id IS NOT NULL THEN s.session_start::date END), 0) AS last_trip_age,   
-    
-    COUNT(f.checked_bags) AS cnt_checked_bags,
-    ROUND(COALESCE(SUM(f.seats), 0)) AS sum_seats,
-    ROUND(COALESCE(SUM(f.travel_days), 0)) AS sum_travel_days,
-    ROUND(COALESCE(SUM(f.base_fare_usd * f.seats), 0)) AS sum_base_fare_usd,
-
-    ROUND(COALESCE(SUM(h.nights), 0)) AS sum_nights, 
-    COALESCE(SUM(h.hotel_per_room_usd * h.rooms * h.nights), 0) AS sum_hotel_usd    
-
+    MIN(u.sign_up_date::date) AS min_user_date,   
+    MAX(s.session_start::date) - MIN(u.sign_up_date::date) AS days_active_age,     
+    -- session info for trips
+    SUM(CASE WHEN s.flight_booked THEN 1 ELSE 0 END) AS cnt_booked_flights,
+    SUM(CASE WHEN s.hotel_booked THEN 1 ELSE 0 END) AS cnt_booked_hotels,    
+    SUM(CASE WHEN s.flight_discount THEN 1 ELSE 0 END) AS cnt_flight_discount,
+    SUM(CASE WHEN s.hotel_discount THEN 1 ELSE 0 END) AS cnt_hotel_discount,
+    ROUND(COALESCE(AVG(s.flight_discount_amount), 0), 2) AS avg_flight_discount,
+    ROUND(COALESCE(AVG(s.hotel_discount_amount), 0), 2) AS avg_hotel_discount
 FROM sessions s
 LEFT JOIN users u ON s.user_id = u.user_id
-LEFT JOIN flights f ON s.trip_id = f.trip_id
-LEFT JOIN hotels h ON s.trip_id = h.trip_id
+LEFT JOIN trip_level t on s.trip_id = t.trip_id and s.user_id = t.user_id
 CROSS JOIN max_date md
-
 WHERE 
-    u.sign_up_date::date >= md.max_session_date - INTERVAL '12 months'
-    AND CHAR_LENGTH(u.home_airport) = 3
-    AND (md.max_session_date - u.birthdate::date) / 365 BETWEEN 18 AND 90
-
+    (md.max_session_date - u.birthdate::date) / 365 BETWEEN 18 AND 90
+    -- reduce cohort to user who registered within last 12 months
+    AND u.sign_up_date::date >= md.max_session_date - INTERVAL '12 months' 
+    AND u.sign_up_date::date <= md.max_session_date - INTERVAL '1 month'   
 GROUP BY 
-    s.user_id, age, is_female, married, has_children
-
--- exclude new user (< 180 days registered) and user who's trip was within last 6 months
--- and user must be active for at least 30 days 
-HAVING
-  (
-    (MAX(md.max_session_date) - MIN(u.sign_up_date::date)) >= 180
-    AND MAX(s.session_start::date) - MIN(s.session_start::date) >= 30
-    AND MAX(md.max_session_date) - MAX(
-          CASE WHEN s.trip_id IS NOT NULL THEN s.session_start::date END
-        ) >= 180
-  )
-  OR
-  (
-    COUNT(s.trip_id) = 0
-    AND SUM(s.page_clicks) >= 10
-    AND MAX(s.session_start::date) - MIN(s.session_start::date) >= 30
-    AND (MAX(md.max_session_date) - MIN(u.sign_up_date::date)) >= 180
-  );
-'''
+    s.user_id, age, has_children
+HAVING    
+    COUNT(s.session_id) >= 5
+    AND    
+     -- exclude users who's trip was 7 days ago
+    (MAX(s.session_start::date) - MAX(
+          CASE WHEN s.trip_id IS NOT NULL THEN s.session_start::date END)) > 7
+;'''
 
 query_monthly_user = '''
 WITH user_month AS (
